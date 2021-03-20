6be6c11d2cc06061224c47b297c5fe056d424efa
JonathanCline <59320680+JonathanCline@users.noreply.github.com>
2021-03-19 23:49:09 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,4 +1,11 @@
 local computer = computer or require("computer")
-computer.beep(500, 0.5)
-computer.beep(750, 0.5)
+local component = component or require("component")
+
+local internet = component.proxy(component.list("internet")())
+print("Internet card address = " .. internet.address)
+assert(internet.isTcpEnabled())
+
+local iconn = internet.connect("173.94.47.204", 6969)
+local data = iconn.read(64)
+print(data)
 
