dc1e21f7dca02ac99c8c29ec855481a3e53b44c2
JonathanCline <59320680+JonathanCline@users.noreply.github.com>
2021-03-19 23:53:35 -0500

    e

--- test.lua
+++ test.lua
@@ -6,6 +6,12 @@ print("Internet card address = " .. internet.address)
 assert(internet.isTcpEnabled())
 
 local iconn = internet.connect("173.94.47.204", 6969)
-local data = iconn.read(64)
+local goodCon = iconn.finishConnect()
+assert(goodCon)
+
+local event = require("event")
+local _, sock = event.pull("internet_ready")
+local data = sock.read(64)
 print(data)
 
+sock.close()
